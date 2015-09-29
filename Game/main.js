3a9927fe4c6dc028a83b4bbf461295f1a2305c62
DamienValenta98 <DValenta98@outlook.com>
2015-09-29 16:52:07 +1000

    Day 2
    changes in all files

--- Game/main.js
+++ Game/main.js
@@ -25,11 +25,49 @@ var fps = 0;
 var fpsCount = 0;
 var fpsTime = 0;
 
-var chuckNorris = document.createElement("img");
-chuckNorris.src = "hero.png";
+var return_cells = [];
+function initialize(input_level)
+{
+	var return_cells = [];
+	
+	for (var layerIdx = 0; layerIdx < LAYER_COUNT; layerIdx++)
+	{
+			return_cells[layerIdx] = [];
+			var idx = 0;
+			for (var y = 0; y < input_level.layers[layerIdx].height; y++)
+			{
+				return_cells[layerIdx][y] = []
+				for (var x = 0; x < input_level.layers[layerIdx].width; x++)
+				{
+					if (input_level.layers[layerIdx].data[idx] != 0)
+					{
+						return_cells[layerIdx][y][x] = 1;
+						return_cells[layerIdx][y][x+1] = 1;
+						if (y != 0)
+						{
+						return_cells[layerIdx][y-1][x+1] = 1;
+						return_cells[layerIdx][y-1][x] = 1;
+						}
+						
+					}
+					else if (return_cells[layerIdx][y][x] = 0)
+					{
+						return_cells[layerIdx][y][x] = 0;
+					}
+					idx++;
+				}
+			}
+	}
+	return return_cells;
+}
 
-var player = new Player();
+var cells = initialize(level);
+
 var keyboard = new Keyboard();
+var player = new Player();
+
+var cam_x = 0;
+var cam_y = 0;
 
 function run()
 {
@@ -38,10 +76,23 @@ function run()
 	
 	var deltaTime = getDeltaTime();
 	
-	drawMap();
+	cam_x = player.x - SCREEN_WIDTH / 2;
+	cam_y = player.y - SCREEN_HEIGHT / 2;
+	
+	if(cam_x < 0)
+		cam_x = 0;
+	if(cam_y < 0)
+		cam_y = 0;
+	
+	if(cam_x > MAP.tw * TILE - SCREEN_WIDTH)
+		cam_x > MAP.tw * TILE - SCREEN_WIDTH;
+	if(cam_y  > MAP.th * TILE - SCREEN_HEIGHT)
+		cam_y > MAP.th * TILE - SCREEN_HEIGHT;
+	
+	drawMap(cam_x, cam_y);
 	
 	player.update(deltaTime);
-	player.draw();
+	player.draw(cam_x, cam_y);
 	
 		// update the frame counter
 	fpsTime += deltaTime;
